f90271b4134447a580a5a59f97f46c5a3c7428ad
Nachiappan <nachiapan@gmail.com>
2022-02-06 21:17:21 +0530

    Skeleton project

---
+++ LearningAspNetCore/LearningAspNetCore/Controllers/LearningController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningAspNetCore.Controllers
+{
+    [ApiController]
+    [Route("/api/learning-controller")]
+    public class LearningController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "hi";
+        }
+    }
+}
