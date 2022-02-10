c53db7770c610a18798a82a162c68323f649b46a
Subramanian N <Subramanian.N@vsoftwareconsulting.com>
2022-02-09 20:36:16 -0600

    raw string

--- LearningAspNetCore/LearningAspNetCore/Controllers/LearningController.cs
+++ LearningAspNetCore/LearningAspNetCore/Controllers/LearningController.cs
@@ -2,7 +2,9 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace LearningAspNetCore.Controllers
@@ -22,8 +24,23 @@ namespace LearningAspNetCore.Controllers
         [Route("/api/learning-controller/bodystring")]
         public string JsonStringBody([FromBody] string content)
         {
+            // Note that the string sent is not a raw string, but rather a JSON string as it includes the wrapping quotes:
             return content;
         }
+
+
+        [HttpPost]
+        [Route("/api/learning-controller/ReadStringDataManual")]
+        public async Task<string> ReadStringDataManual()
+        {
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                return await reader.ReadToEndAsync();
+            }
+
+
+        }
+
 
 
     }
