e0acf61bd6525c3f5aa3a282383cf50d77edd3a1
Subramanian N <subramanian.nk@live.com>
2022-02-12 05:51:14 -0600

    method as outputs

--- LearningAspNetCore/LearningAspNetCore/Controllers/LearningController.cs
+++ LearningAspNetCore/LearningAspNetCore/Controllers/LearningController.cs
@@ -16,5 +16,30 @@ namespace LearningAspNetCore.Controllers
         {
             return "hi";
         }
+
+        [HttpDelete("delete")]
+        public string GetG()
+        {
+            return Request.Method;
+        }
+
+        [HttpPut("put")]
+        public string GetDel()
+        {
+            return Request.Method;
+        }
+
+        [HttpPost("post")]
+        public string GetPost()
+        {
+            return Request.Method;
+        }
+
+        [HttpGet("get")]
+        public string GetGet()
+        {
+            return Request.Method;
+        }
+
     }
 }
